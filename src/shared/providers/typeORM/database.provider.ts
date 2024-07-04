212963351cd5b2be1c1294910d433742d43bd967
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2024-07-04 17:39:07 -0300

    feat: Add databaseProvider

--- src/shared/providers/typeORM/database.provider.ts
+++ src/shared/providers/typeORM/database.provider.ts
@@ -1,16 +1,19 @@
+import { User } from 'src/modules/user/infra/entities/user.entity';
 import { DataSource } from 'typeorm';
+import 'dotenv/config';
+import { CreateUsers } from './migrations/createUsers.migration';
 
 export const DatabaseProvider = {
   provide: 'DATA_SOURCE',
   useFactory: async () => {
     const dataSource = new DataSource({
       type: 'postgres',
-      host: process.env.HOST,
       port: Number(process.env.DATABASE_PORT),
       username: process.env.USER,
       password: process.env.PASSWORD,
       database: process.env.DATABASE,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      entities: [User],
+      migrations: [CreateUsers],
       synchronize: false,
     });
 
