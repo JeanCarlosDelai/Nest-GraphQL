9828714bc267075485b9a27b9813bca4de71ee04
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2024-07-05 16:59:21 -0300

    feat: Add ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from './shared/config/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true, disableErrorMessages: false })
+  );
   await app.listen(config.PORT);
   logger.log(`API Nest-GrapghQl it ruuning: ${config.PORT}`);
 }
