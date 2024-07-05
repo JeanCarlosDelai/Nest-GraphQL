b7c3f13391a049c7b33d6a7e259ed0ca419076c9
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2024-07-05 16:55:00 -0300

    feat: Add updateUserInput

---
+++ src/modules/user/domain/dtos/updateUserInput.dto.ts
@@ -0,0 +1,16 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+@InputType()
+export class UpdateUserInputDto {
+  @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  readonly name?: string;
+
+  @Field({ nullable: true })
+  @IsString()
+  @IsEmail()
+  @IsOptional()
+  readonly email?: string;
+}
