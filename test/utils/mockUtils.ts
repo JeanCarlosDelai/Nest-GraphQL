2cfcb57bd83d06c02991950e483365827c9e1334
JeanCarlosDelai <Jean_vieceli@outlook.com>
2024-07-06 14:43:35 -0300

    feat: Add mockUtils

---
+++ test/utils/mockUtils.ts
@@ -0,0 +1,14 @@
+import { User } from 'src/modules/user/infra/entities/user.entity';
+
+export class MockUtils {
+  static validUser(): User {
+    const user: User = {
+      id: '1',
+      name: 'jean',
+      email: 'teste@gmail.com',
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+    return user;
+  }
+}
